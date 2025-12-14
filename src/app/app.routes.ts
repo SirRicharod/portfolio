9bba629fd4bcf824a76c26ba8d85d97f43cd5c76
riccardog <riccardog.education@gmail.com>
2025-12-14 12:05:12 +0100

    Initialized portfolio. Setup components/header + footer. Setup pages/home + about + projects + resume. Routed pages and added Bootstrap!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Projects } from './pages/projects/projects';
+import { About } from './pages/about/about';
+import { Resume } from './pages/resume/resume';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'projects', component: Projects},
+    {path: 'about', component: About},
+    {path: 'resume', component: Resume}
+];
