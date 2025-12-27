1cb763aa6cd33279e1e380525925b2e6f72bccb0
riccardog <riccardog.education@gmail.com>
2025-12-27 13:08:12 +0100

    redid the general theming of the site: -removed footer -changed navbar to clean minimalistic -added a color palette -edited homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./component/header/header";
-import { Footer } from "./component/footer/footer";
+// import { Footer } from "./component/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header /*,Footer*/],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
