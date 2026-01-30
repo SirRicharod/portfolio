16d0267fda8761220e6c21be43b90fec86d72a9f
SirRicharod <riccardo.contact@proton.me>
2026-01-30 15:35:25 +0100

    enhance about page responsiveness

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ export const routes: Routes = [
   { path: 'projects', loadComponent: () => import('./pages/projects/projects').then(m => m.Projects), data: { animation: 'ProjectsPage' } },
   { path: 'about', loadComponent: () => import('./pages/about/about').then(m => m.About), data: { animation: 'AboutPage' } },
   { path: 'resume', loadComponent: () => import('./pages/resume/resume').then(m => m.Resume), data: { animation: 'ResumePage' } },
+  { path: 'contact', loadComponent: () => import('./pages/contact/contact').then(m => m.Contact), data: { animation: 'ContactPage' } },
   { path: '**', loadComponent: () => import('./component/error404/error404').then(m => m.Error404), data: { animation: 'ErrorPage' } },
 ];
 
--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface Skill {
   name: string;
@@ -18,43 +18,48 @@ interface Category {
   styleUrl: './about.css',
 })
 export class About {
-  public categories: Category[] = [
-    {
-      title: "Frontend",
-      skills:
-        [
-          { name: 'Angular', icon: 'fa-brands fa-angular' },
-          { name: 'Bootstrap', icon: 'bi-bootstrap-fill' },
-          { name: 'TypeScript', icon: 'bi bi-typescript' },
-          { name: 'JavaScript', icon: 'fa-brands fa-js' },
-          { name: 'HTML5', icon: 'fa-brands fa-html5' },
-          { name: 'CSS3', icon: 'fa-brands fa-css3-alt' },
-        ]
-    },
-    {
-      title: "Backend",
-      skills: [
-        { name: 'Laravel', icon: null, image: 'laravel.png' },
-        { name: 'PHP', icon: null, image: 'php.png' },
-        { name: 'Python', icon: null, image: 'python.png' },
 
+birthdate = new Date("2001-08-13T00:00:00");
+timeDiff = Math.abs(Date.now() - this.birthdate.getTime());
+age = signal<number>(Math.floor((this.timeDiff / (1000 * 3600 * 24)) / 365.25));
+
+  public categories: Category[] = [
+  {
+    title: "Frontend",
+    skills:
+      [
+        { name: 'Angular', icon: 'fa-brands fa-angular' },
+        { name: 'Bootstrap', icon: 'bi-bootstrap-fill' },
+        { name: 'TypeScript', icon: 'bi bi-typescript' },
+        { name: 'JavaScript', icon: 'fa-brands fa-js' },
+        { name: 'HTML5', icon: 'fa-brands fa-html5' },
+        { name: 'CSS3', icon: 'fa-brands fa-css3-alt' },
       ]
-    },
-    {
-      title: "3D & Gaming",
-      skills: [
-        { name: 'Unity', icon: 'fa-brands fa-unity' },
-        { name: 'Unreal Engine 5', icon: null, image: 'ue5.png' },
-        { name: 'C#', icon: null, image: 'csharp.png' },
-        { name: 'Blender', icon: null, image: 'blender.png' }]
-    },
-    {
-      title: "Design",
-      skills: [
-        { name: 'Affinity', icon: null, image: 'affinity.png' },
-        { name: 'Aseprite', icon: null, image: 'aseprite.png' },
-        { name: 'Procreate', icon: null, image: 'procreate.png' },
-      ]
-    }
-  ]
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: 'Laravel', icon: null, image: 'laravel.png' },
+      { name: 'PHP', icon: null, image: 'php.png' },
+      { name: 'Python', icon: null, image: 'python.png' },
+
+    ]
+  },
+  {
+    title: "3D & Gaming",
+    skills: [
+      { name: 'Unity', icon: 'fa-brands fa-unity' },
+      { name: 'Unreal Engine 5', icon: null, image: 'ue5.png' },
+      { name: 'C#', icon: null, image: 'csharp.png' },
+      { name: 'Blender', icon: null, image: 'blender.png' }]
+  },
+  {
+    title: "Design",
+    skills: [
+      { name: 'Affinity', icon: null, image: 'affinity.png' },
+      { name: 'Aseprite', icon: null, image: 'aseprite.png' },
+      { name: 'Procreate', icon: null, image: 'procreate.png' },
+    ]
+  }
+]
 }
