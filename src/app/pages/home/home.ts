481c9becb07df206ea780f6ce100d11d0bfeaffb
SirRicharod <riccardo.contact@proton.me>
2026-01-30 22:29:12 +0100

    feat: add Angular CDK dependency and implement social media icons
    - Added @angular/cdk dependency to package.json.
    - Updated home.css to enhance section styling and added social box styles with hover effects.
    - Modified home.html to include social media icons with links.
    - Implemented triggerRainbow method in home.ts to animate text on mouse enter.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -9,4 +9,21 @@ import { RouterLink } from "@angular/router";
 })
 export class Home {
 
+  public socials = [
+    { name: 'LinkedIn', icon: 'bi-linkedin', url: 'https://www.linkedin.com/in/riccardo-giannecchini' },
+    { name: 'GitHub', icon: 'bi-github', url: 'https://github.com/SirRicharod' },
+    { name: 'Email', icon: 'bi-envelope-fill', url: 'mailto:riccardo.contact@proton.me' }
+  ];
+
+  triggerRainbow(event: MouseEvent) {
+    const element = event.target as HTMLElement;
+
+    if (!element.classList.contains('animate-rainbow')) {
+      element.classList.add('animate-rainbow');
+
+      setTimeout(() => {
+        element.classList.remove('animate-rainbow');
+      }, 3000);
+    }
+  }
 }
