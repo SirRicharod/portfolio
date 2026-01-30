1bdcfe82ac76f715f43421b8f7de39c7c62bdf08
SirRicharod <riccardo.contact@proton.me>
2026-01-30 22:37:24 +0100

    feat: implement contact section with dynamic contact items and styles

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,25 @@
+// contact.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface ContactInfo {
+  label: string;
+  value: string;
+  icon: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
-
+  public contacts: ContactInfo[] = [
+    { label: 'Email', value: 'riccardo.contact@proton.me', icon: 'bi-envelope-at', url: 'mailto:riccardo.contact@proton.me' },
+    { label: 'LinkedIn', value: 'Riccardo Giannecchini', icon: 'bi-linkedin', url: 'https://linkedin.com/in/riccardo-giannecchini' },
+    { label: 'GitHub', value: '@SirRicharod', icon: 'bi-github', url: 'https://github.com/SirRicharod' }
+  ];
 }
